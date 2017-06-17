b9f39be3776959f9519554abf39d413e4b33c727
michalkalkowski <kalkowski.m@gmail.com>
2017-06-17 18:58:59 +0100

    fixed torsional wave in the pipe in soil

--- CAS_paper_validation/Muggleton_torsional_paper_spectral.py
+++ CAS_paper_validation/Muggleton_torsional_paper_spectral.py
@@ -11,48 +11,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('/home/michal/Dropbox/plot_templates/default.mplstyle')
 
-import SAFE_mesh as sm
-import SAFE_core
-import SAFE_plot
-import Misc
+from context import axisafe
 #%%
-lame_1, lame_2 = Misc.young2lame(156e9, 0.2)
+lame_1, lame_2 = axisafe.misc.young2lame(156e9, 0.2)
 cast_iron = [lame_1, lame_2, 7800, 0.0]
-lame_1i, lame_2i = Misc.young2lame(52000000.0, 0.3)
+lame_1i, lame_2i = axisafe.misc.young2lame(52000000.0, 0.3)
 soil = [lame_1i, lame_2i, 2000, 0.]
 properties = dict([('cast_iron', cast_iron), ('soil', soil)])
 
 h_long, h_short, PML_order, R_s, R_l, R_sl= \
-        sm.suggest_PML_parameters(0.105, 8, properties['cast_iron'], 
+        axisafe.mesh.suggest_PML_parameters(0.105, 8, properties['cast_iron'], 
                                   properties['soil'], 1, 5e3, att=1)
 
 sets = [['cast_iron', 0.095, 0.01, cast_iron, 'SLAX6'], 
-        ['soil', 0.105, h_long/16, soil, 'SLAX6_PML', PML_order, 1]] #0.3
+        ['soil', 0.105, h_long/16, soil, 'SLAX6_PML', PML_order, 1]] 
         
-PML = dict([('PML', [0.105, h_long/16, 6 + 7j])])
-
 f = np.logspace(-1, 3.7, 50, True)
 #%%
-mesh0 = sm.Mesh()
-mesh0.create(sets, f[-1], 0, 'soil')
-
-mesh0.assign_properties(properties)
-mesh0.set_PML_parameters(PML)
-
+mesh0 = axisafe.mesh.Mesh()
+mesh0.create(sets, f[-1], 0, 'soil', PML_props=[0.105, h_long/16, 6 + 7j])
 mesh0.assemble_matrices(n=0)
 
-pipe0 = SAFE_core.WaveElementAxisym(mesh0)
-
-pipe0.linearise()
-
+pipe0 = axisafe.solver.WaveElementAxisym(mesh0)
 pipe0.solve(f, no_of_waves='full',
                     central_wavespeed=((cast_iron[0] + 2*cast_iron[1])/cast_iron[2])**0.5)
-pipe0.sort()
 pipe0.energy_ratio()
 pipe0.k_propagating(20, 0.95)
-pipe0.trace_back([0, 1], 200)
 
-k_0 = np.copy(pipe0.k_ready)
+k_0 = pipe0.k[:, pipe0.propagating_indices]
 
 #%% From Jen Muggleton's torsional waves paper in JSV 2016
 Jen_r = np.genfromtxt('/home/michal/mCloud/ATU/torsional_waves_Jen/results/Jens/Rek_Jen.txt')
@@ -77,11 +63,5 @@ for ax in [ax1, ax2]:
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
 plt.tight_layout()
-plt.savefig('torsional.pdf', dpi=600)
 plt.show()
 #%% 
-# Look at the wave mode shapes at low frequencies
-T = np.tile(pipe0.T.toarray().conj(), (len(pipe0.w), 1, 1))
-q = np.matmul(T, pipe0.psi_hat[:, pipe0.Mesh.no_of_dofs:])
-SAFE_plot.animate_wave(q[10, :, 495], mesh0.glob_nodes, mesh0.ID)
-# I checked - what i identify as torsional is in fact torsional
